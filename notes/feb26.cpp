555670f653c6968d1bd0f380bf87b926088fe047
Josh Douglas <123434357+Dread63@users.noreply.github.com>
2025-03-10 07:58:31 -0600

    Feb 26 Notes

---
+++ notes/feb26.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+struct test{
+
+    int data;
+};
+
+int main() {
+
+    using namespace std;
+
+    char testChar = 'a';
+
+    char upperTestChar = toupper(testChar);
+
+    cout << upperTestChar << endl;
+
+    struct data = new test;
+    struct data2 = new test;
+
+    bool operator==(const MyStruct& other) const {
+
+        return (x == other.x && y == other.y)
+    }
+
+    return 0;
+}
